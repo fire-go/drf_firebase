8a1ca787165f9dc9142c6a26351fe1adda1022d1
Ahmane Samir <samirahmane.trading@gmail.com>
2022-01-27 13:04:21 +0100

    feat: coded authenticate_credentials

--- drf_firebase/authentication.py
+++ drf_firebase/authentication.py
@@ -1,4 +1,4 @@
-from rest_framework import authentication
+from rest_framework import authentication, exceptions
 from .settings import api_settings
 from firebase_admin import auth as firebase_auth
 from typing import Tuple, Dict
@@ -12,8 +12,8 @@ class FirebaseAuthentication(authentication.TokenAuthentication):
     """
     Firebase token based authentication.
     Clients should authenticate by passing the token keyword in the "Authorization"
-    HTTP header, prepended with the string "Keyword " mentionned in the environment 
-    variable FIREBASE_AUTH_HEADER_PREFIX which has the default value "Bearer ".  
+    HTTP header, prepended with the string "Keyword " mentionned in the environment
+    variable FIREBASE_AUTH_HEADER_PREFIX which has the default value "Bearer ".
     For example:
         -Authorization: Bearer 401f7ac837da42b97f613d789819ff93537bee6a
     """
@@ -21,7 +21,13 @@ class FirebaseAuthentication(authentication.TokenAuthentication):
     keyword = api_settings.FIREBASE_AUTH_HEADER_PREFIX
 
     def authenticate_credentials(self, token: str) -> Tuple[AnonymousUser, Dict]:
-        pass
+        try:
+            decoded_token = self._decode_token(token)
+            firebase_user = self._authenticate_token(decoded_token)
+            local_user = self._get_or_create_local_user(firebase_user)
+            return (local_user, decoded_token)
+        except Exception as e:
+            raise exceptions.AuthenticationFailed(e)
 
     def _decode_token(self, token: str) -> Dict:
         pass
