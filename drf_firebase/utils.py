492114940c53ca46aa38221e584740395bb57082
Ahmane Samir <samirahmane.trading@gmail.com>
2022-01-27 10:28:08 +0100

    feat: created get_firebase_identifier

--- drf_firebase/utils.py
+++ drf_firebase/utils.py
@@ -6,7 +6,7 @@ def get_firebase_user_uid(firebase_user: auth.UserRecord) -> str:
     See Official documentation: shorturl.at/irBS3
     parameters:
     -firebase_user: firebase_admin.auth.UserRecord
-       
+
     return: Firebase Unique Identifier Aka UID
     """
     try:
@@ -16,3 +16,18 @@ def get_firebase_user_uid(firebase_user: auth.UserRecord) -> str:
         raise Exception(e)
 
 
+def get_firebase_user_identifier(firebase_user: auth.UserRecord) -> str:
+    try:
+        return (
+            firebase_user.email
+            if firebase_user.email
+            else firebase_user.provider_data[0].email
+            if firebase_user.provider_data[0].email
+            else firebase_user.phone_number
+            if firebase_user.phone_number
+            else firebase_user.provider_data[0].phone_number
+            if firebase_user.provider_data[0].phone_number
+            else Exception("Identifier not found.")
+        )
+    except Exception as e:
+        raise Exception(e)
