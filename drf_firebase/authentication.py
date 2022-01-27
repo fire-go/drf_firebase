a9839232debd3bee9bf10a5cc0ac9c79a56aa5e0
Ahmane Samir <samirahmane.trading@gmail.com>
2022-01-27 11:10:04 +0100

    feat: created expected functions for FirebaseAuthentication class

--- drf_firebase/authentication.py
+++ drf_firebase/authentication.py
@@ -1,5 +1,26 @@
 from rest_framework import authentication
+from .settings import api_settings
+from firebase_admin import auth as firebase_auth
+from typing import Tuple, Dict
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class FirebaseAuthentication(authentication.TokenAuthentication):
-    pass
+    keyword = api_settings.FIREBASE_AUTH_HEADER_PREFIX
+
+    def authenticate_credentials(self, token: str) -> Tuple[AnonymousUser, Dict]:
+        pass
+
+    def _decode_token(self, token: str) -> Dict:
+        pass
+
+    def _authenticate_token(self, decoded_token: Dict) -> firebase_auth.UserRecord:
+        pass
+
+    def _get_or_create_local_user(
+        self, firebase_user: firebase_auth.UserRecord
+    ) -> User:
+        pass
