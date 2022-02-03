0d44b216a45f0cffe1f937b711f1a2c6f5a07ea9
Samir Ahmane <samirahmane.trading@gmail.com>
2022-02-03 12:05:57 +0100

    chores: format with black

--- drf_firebase/__init__.py
+++ drf_firebase/__init__.py
@@ -1,15 +1,15 @@
 """Metadata for drf-firebase package."""
-__title__ = 'drf-firebase'
-__version__ = '0.0.1'
+__title__ = "drf-firebase"
+__version__ = "0.0.1"
 __description__ = (
-    'Custom Django Rest Framework authentication backend for '
-    'parsing Firebase uid tokens and storing as local users.'
+    "Custom Django Rest Framework authentication backend for "
+    "parsing Firebase uid tokens and storing as local users."
 )
-__url__ = 'https://github.com/fire-go/drf_firebase'
-__author__ = 'Firego'
-__author_email__ = 'samirahmane.trading@gmail.com'
-__license__ = 'MIT'
-__copyright__ = 'FirePy'
+__url__ = "https://github.com/fire-go/drf_firebase"
+__author__ = "Firego"
+__author_email__ = "samirahmane.trading@gmail.com"
+__license__ = "MIT"
+__copyright__ = "FirePy"
 
 # Version synonym
 VERSION = __version__
