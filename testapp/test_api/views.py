55cb8ea03dc8ef16c5309f1f522702277903f914
Samir Ahmane <samirahmane.trading@gmail.com>
2022-02-04 12:57:46 +0100

    feat: created get_user view

--- testapp/test_api/views.py
+++ testapp/test_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from uritemplate import partial
+from .serializers import UserSerializer
 
 # Create your views here.
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    user = User.objects.get(username=request.user)
+    serializer = UserSerializer(user)
+    return Response(status=200, data=serializer.data)
